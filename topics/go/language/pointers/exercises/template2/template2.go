5a94d6ca7314518cfbf8c0c92bdecb12f9a59e3f
JohnnyB0Y <ag860050872@163.com>
2021-06-27 21:54:28 +0800

    pointers exercise2 done.

--- topics/go/language/pointers/exercises/template2/template2.go
+++ topics/go/language/pointers/exercises/template2/template2.go
@@ -6,24 +6,38 @@
 // value before and after the call to your function.
 package main
 
+import "fmt"
+
 // Add imports.
 
 // Declare a type named user.
+type user struct {
+	Name string
+	Age  int
+}
 
 // Create a function that changes the value of one of the user fields.
-func funcName( /* add pointer parameter, add value parameter */ ) {
+func funcName( /* add pointer parameter, add value parameter */ user *user, name string) {
 
 	// Use the pointer to change the value that the
 	// pointer points to.
+	user.Name = name
 }
 
 func main() {
 
 	// Create a variable of type user and initialize each field.
+	jack := user{
+		Name: "Jack",
+		Age:  24,
+	}
 
 	// Display the value of the variable.
+	fmt.Println(jack.Name, jack.Age)
 
 	// Share the variable with the function you declared above.
+	funcName(&jack, "Jackson")
 
 	// Display the value of the variable.
+	fmt.Println(jack.Name, jack.Age)
 }
